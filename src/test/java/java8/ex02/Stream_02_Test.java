b3b9dd70ec7ec7cd8dc11667b69c8d190cd48034
Gleamoh <ketwenty@KM-YOGA14.localdomain>
2018-03-14 00:18:39 +0100

    exo 2 done

--- src/test/java/java8/ex02/Stream_02_Test.java
+++ src/test/java/java8/ex02/Stream_02_Test.java
@@ -1,47 +1,62 @@
 package java8.ex02;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+
 import java8.data.Data;
 import java8.data.domain.Customer;
 import java8.data.domain.Order;
 import java8.data.domain.Pizza;
-import org.junit.Test;
-
-import java.util.IntSummaryStatistics;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.*;
 
 /**
  * Exercice 02 - Transformation
  */
 public class Stream_02_Test {
 
-    @Test
-    public void test_map() throws Exception {
+	@Test
+	public void test_map() throws Exception {
 
-        List<Order> orders = new Data().getOrders();
+		List<Order> orders = new Data().getOrders();
 
-        // Trouver la liste des clients ayant déjà passés une commande
-        List<Customer> result = orders.stream().filter(o -> o.getCustomer() != null).map(o -> o.getCustomer()).collect(Collectors.groupingBy());
+		// Trouver la liste des clients ayant déjà passés une commande
 
-        assertThat(result, hasSize(2));
-    }
+		// grouper les orders par client
+		Map<Customer, List<Order>> customerOrders = orders.stream().collect(Collectors.groupingBy(Order::getCustomer));
 
-    @Test
-    public void test_flatmap() throws Exception {
+		// recuper la listes des clients
+		List<Customer> result = customerOrders.keySet().stream().collect(Collectors.toList());
 
-        List<Order> orders = new Data().getOrders();
+		assertThat(result, hasSize(2));
+	}
 
-        // calculer les statistiques sur les prix des pizzas vendues
-        // utiliser l'opération summaryStatistics
-        IntSummaryStatistics result = null;
+	@Test
+	public void test_flatmap() throws Exception {
 
+		List<Order> orders = new Data().getOrders();
 
-        assertThat(result.getSum(), is(10900L));
-        assertThat(result.getMin(), is(1000));
-        assertThat(result.getMax(), is(1375));
-        assertThat(result.getCount(), is(9L));
-    }
+		// calculer les statistiques sur les prix des pizzas vendues
+
+		// appique un flatmap pour fusion la liste de liste de pizza en une
+		// seule liste de pizza
+		List<Pizza> pizzas = orders.stream().map(Order::getPizzas).flatMap(list -> list.stream())
+				.collect(Collectors.toList());
+		
+		// utiliser l'opération summaryStatistics
+		IntSummaryStatistics result = pizzas.stream().collect(Collectors.summarizingInt(Pizza::getPrice));
+
+		assertThat(result.getSum(), is(10900L));
+		assertThat(result.getMin(), is(1000));
+		assertThat(result.getMax(), is(1375));
+		assertThat(result.getCount(), is(9L));
+	}
 }
